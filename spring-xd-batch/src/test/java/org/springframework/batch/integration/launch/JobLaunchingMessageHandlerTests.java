4b919383f3badae83ea1216aaf8a5f474e17d64e
Mark Fisher <markfisher@vmware.com>
2013-08-19 13:34:57 -0400

    added new spring-xd-batch module
    currently contains the launch-related code from spring-batch-integration

---
+++ spring-xd-batch/src/test/java/org/springframework/batch/integration/launch/JobLaunchingMessageHandlerTests.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.batch.integration.launch;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.integration.launch.JobLaunchRequest;
+import org.springframework.batch.integration.launch.JobLaunchRequestHandler;
+import org.springframework.batch.integration.launch.JobLaunchingMessageHandler;
+import org.springframework.batch.integration.test.support.JobSupport;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+
+@ContextConfiguration(locations = {
+	"/org/springframework/batch/integration/launch/job-execution-context.xml" })
+public class JobLaunchingMessageHandlerTests extends AbstractJUnit4SpringContextTests {
+
+	JobLaunchRequestHandler messageHandler;
+
+	StubJobLauncher jobLauncher;
+
+	@Before
+	public void setUp() {
+		jobLauncher = new StubJobLauncher();
+		messageHandler = new JobLaunchingMessageHandler(jobLauncher);
+	}
+
+	@Test
+	public void testSimpleDelivery() throws Exception {
+		messageHandler.launch(new JobLaunchRequest(new JobSupport("testjob"), null));
+		assertEquals("Wrong job count", 1, jobLauncher.jobs.size());
+		assertEquals("Wrong job name", jobLauncher.jobs.get(0).getName(), "testjob");
+	}
+
+	private static class StubJobLauncher implements JobLauncher {
+
+		List<Job> jobs = new ArrayList<Job>();
+
+		List<JobParameters> parameters = new ArrayList<JobParameters>();
+
+		AtomicLong jobId = new AtomicLong();
+
+		public JobExecution run(Job job, JobParameters jobParameters) {
+			jobs.add(job);
+			parameters.add(jobParameters);
+			return new JobExecution(new JobInstance(jobId.getAndIncrement(), job.getName()), jobParameters);
+		}
+	}
+
+}
